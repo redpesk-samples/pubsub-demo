5913931ea4fac209f6581207c28eb98e0c47f7a5
Marius CACHELIN <marius.cachelin@iot.bzh>
2023-04-13 14:31:36 +0200

    fix issue with BUILDDIR path

--- start-demo.sh
+++ start-demo.sh
@@ -2,5 +2,8 @@
 
 cd `dirname $0`
 
-(mkdir -p build && cd build && cmake . && make)
+(mkdir -p build && cd build && cmake .. && make && cd ..)
+
+export BUILDDIR=$(pwd)/build;
+
 afb-binder --config=hackaton.config -vvv
