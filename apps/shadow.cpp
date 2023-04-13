49c7eae89b33ede0cf152d1bf24172fe936d93ac
Marius CACHELIN <marius.cachelin@iot.bzh>
2023-04-13 09:07:13 +0200

    add random in shadow app

--- apps/shadow.cpp
+++ apps/shadow.cpp
@@ -19,6 +19,12 @@
 #include <string>
 #include <vector>
 
+struct ShadowData
+{
+	int timestamp;
+	int value;
+};
+
 afb::event data_event;
 
 json_object *json_of_data(const afb::data &d)
@@ -62,11 +68,30 @@ void dispatch(void *closure, const char *event, unsigned n, afb_data_t const a[]
 	struct json_object *r;
 	struct json_object *jtimestamp, *jvalue;
 
+	struct ShadowData data;
+
 	afb::received_data params(n, a);
 	r = json_of_data(params[0]);
 
+	json_object_object_get_ex(r, "timestamp", &jtimestamp);
+	json_object_object_get_ex(r, "value", &jvalue);
+
+	data.timestamp = json_object_get_int(jtimestamp);
+	data.value = json_object_get_int(jvalue);
+
+	if (data.timestamp % 5 == 0)
+	{
+		data.value = data.value + (std::rand() % 100);
+	}
+	json_object *obj;
+
+	// create event data_csv
+	wrap_json_pack(&obj, "{si si}",
+				   "timestamp", data.timestamp,
+				   "value", data.value);
+
 	afb::dataset<1> dataset;
-	dataset[0] = json_to_req_data(NULL, r);
+	dataset[0] = json_to_req_data(NULL, obj);
 	data_event.push(dataset);
 }
 
